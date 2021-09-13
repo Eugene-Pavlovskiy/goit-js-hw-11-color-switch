5c9a6be24887ae1081c5f1631b26b804fdacf21b
Pav_lov_sky <infopavlovskiy@gmail.com>
2021-09-14 01:26:39 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -5,6 +5,49 @@ const colors = [
     '#FF9800',
     '#009688',
     '#795548',
+    '#fa03c5',
+    '#e1fa03',
+    '#fa0303'
   ];
 
-  
+  const refs = {
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+  };
+
+  let intervalId = null;
+  // let isActive = false;
+  // const randomIntegerFromInterval = (min, max) => {
+  //   return Math.floor(Math.random() * (max - min + 1) + min);
+  // };
+
+  // refs.startBtn.addEventListener('click', () = {
+  //   if (isActive) {
+  //     return;
+  //   };
+  //   isActive = true;
+  //   intervalId = setInterval(() => {
+  //     const randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  //     document.body.style.backgroundColor = randomColor;
+  //   }, 1000);
+  // });
+
+  // refs.stopBtn.addEventListener('click', () => {
+  //   clearInterval(intervalId);
+  //   isActive = false;
+  // });
+  refs.startBtn.onclick = () => {
+    refs.startBtn.disabled = true;
+    intervalId = setInterval(() => {
+    document.body.style.backgroundColor = colors[randomId(0, colors.length - 1)];
+  }, 1000);
+};
+
+refs.stopBtn.onclick = () => {
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+};
+
+function randomId(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
